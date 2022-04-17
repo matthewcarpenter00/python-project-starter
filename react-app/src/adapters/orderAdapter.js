6037faeeef2985f4da06bccd054960188574ebe4
Alexson Coelho <coelho@Alexsons-MacBook-Pro-2.local>
2022-04-17 18:00:43 -0400

    create order

--- react-app/src/adapters/orderAdapter.js
+++ react-app/src/adapters/orderAdapter.js
@@ -1,12 +1,12 @@
 export const createOrderDto = (order) => {
   return {
-    docNumber: order.poJobName,
-    totalAmount: 200,
-    invoiceNumber: 3,
-    poName: order.poJobName,
-    shippingRoute: "Tampa",
-    orderStatus: "New",
+    docNumber: order.poName,
+    totalAmount: order.totalOrderAmount,
+    invoiceNumber: parseInt(order.invoiceNumber),
+    poName: order.poName,
+    shippingRoute: order.route.value,
+    orderStatus: order.orderStatus.value,
     staffId: 1,
-    customerId: 1,
+    customerId: order.customer.value.id,
   };
 };
